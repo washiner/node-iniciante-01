9eb5a3e84744d40a7c5b82486ce97ebbf0fd8cf1
Washiner Takeuchi <washiner@gmail.com>
2023-03-07 22:51:04 -0400

    aula de como iniciar com node

--- README.md
+++ README.md
@@ -1,8 +1,27 @@
 # node-iniciante-01
 
 <h2>
-washiner
+
+- crie uma pasta onde vc quiser abra com vscode
+- no terminal de o comando npm init ou npm init -y que ai nao vai ter fazer perguntas e instala direto
+- instalar o express npm install express ou de uma forma curta npm i express
 
 
+exemplo-01----------
+    const express = require('express')
+    const server = express()
+
+    server.get('/projeto', (require, response)=>{
+
+    return response.send("Hello World Washiner")
+
+})
+    server.listen(3000)
+
+exemplo-01-final------------
+
+- o exemplo-01 foi apagado so para exemplo mesmo inicial com entendimento basico se rodar esse codigo ele funciona normalmente 
+
+- alem disso foi instaldo o nodemon com npm install nodemon para server ficar dinamico
 
 </h2>
--- index.js
+++ index.js
@@ -1,1 +1,68 @@
-console.log("Hello World")
+const { response } = require("express");
+const express = require("express");
+const server = express();
+
+server.use(express.json());
+
+let custumers = [
+  {
+    id: 1,
+    name: "Dev Washiner",
+    site: "https://washiner.github.io/portifolio/",
+  },
+  { id: 2, name: "Google", site: "https://www.google.com" },
+  { id: 3, name: "Uol", site: "https://uol.com.br" },
+];
+
+server.get("/custumers", (require, response) => {
+  return response.json(custumers)
+});
+
+server.get("/custumers/:id", (require, response) => {
+     const id = parseInt(require.params.id)        
+     const customer = custumers.find(item => item.id === id)   
+     const status = customer ? 200 : 404
+
+    return response.status(status).json(customer)
+  });
+
+  server.post("/custumers", (require, response) => {
+
+    const {name, site} = require.body
+    const id = custumers[custumers.length -1].id + 1
+
+    const newCustomer = {id, name, site}
+    custumers.push(newCustomer)
+
+    return response.status(201).json(newCustomer)
+
+  })
+
+    server.put("/custumers/:id", (require, response) => {
+        const id = parseInt(require.params.id)
+        const {name, site} = require.body
+
+        const index = custumers.findIndex(item => item.id === id)
+        const status = index >= 0 ? 200 : 404
+
+        if(index >= 0) {
+            custumers[index] = {id: parseInt(id), name, site}
+        }
+        return response.status(status).json(custumers[index])
+    })
+
+    server.delete("/custumers/:id", (require, response) => {
+        const id = parseInt(require.params.id)
+        const index = custumers.findIndex(item => item.id === id)
+        const status = index >= 0 ? 200 : 404
+
+        if(index >= 0){
+            custumers.splice(index, 1)
+        }
+
+        return response.status(status).json()
+
+        
+    })
+
+server.listen(3000);
